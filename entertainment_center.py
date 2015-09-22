ae05c67da6e4cd433566d16bd5a7c2cc95d5efec
eduartua <eduartua@gmail.com>
2015-09-22 17:32:43 -0500

    Fixing Python style errors according to the PEP8.

--- entertainment_center.py
+++ entertainment_center.py
@@ -6,28 +6,31 @@ import fresh_tomatoes
 # Each one below is an instance of a movie.
 
 robocop = media.Movie("RoboCop", "A superhuman cyborg known as RoboCop.",
-                      "https://upload.wikimedia.org/wikipedia/en/thumb/5/50/Robocop_film.jpg/220px-Robocop_film.jpg",
+                      "https://upload.wikimedia.org/wikipedia/en/thumb/5/50/Robocop_film.jpg/220px-Robocop_film.jpg",  # noqa
                       "www.youtube.com/watch?v=zbCbwP6ibR4")
 
 sixth_sense = media.Movie("The Six Sense", "A marine on an alien planet.",
-                          "https://upload.wikimedia.org/wikipedia/en/6/66/The_sixth_sense.jpg",
+                          "https://upload.wikimedia.org/wikipedia/en/6/66/The_sixth_sense.jpg",  # noqa
                           "www.youtube.com/watch?v=VG9AGf66tXM")
 
 starwars = media.Movie("Star Wars", "A battle in the outer space.",
-                       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Vua_fg3Ut27HGrK5m2VNlJ-b4lqPI45FjymPZFuACg8SMEDb",
+                       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Vua_fg3Ut27HGrK5m2VNlJ-b4lqPI45FjymPZFuACg8SMEDb",  # noqa
                        "https://www.youtube.com/watch?v=vZ734NWnAHA")
 
-a_beautiful_mind = media.Movie("A Beautiful Mind", "The life of John Forbes Nash Jr.",
-                               "https://upload.wikimedia.org/wikipedia/en/b/b8/A_Beautiful_Mind_Poster.jpg",
+a_beautiful_mind = media.Movie("A Beautiful Mind", "The life of John Forbes Na\
+                               sh Jr.",
+                               "https://upload.wikimedia.org/wikipedia/en/b/b8/A_Beautiful_Mind_Poster.jpg",  # noqa
                                "https://www.youtube.com/watch?v=WFJgUm7iOKw")
 
 others = media.Movie("The Others", "The story of a ghost family",
-                     "https://upload.wikimedia.org/wikipedia/en/4/4c/TheOthers.jpg",
+                     "https://upload.wikimedia.org/wikipedia/en/4/4c/TheOthers.jpg",  # noqa
                      "https://www.youtube.com/watch?v=0bMEGtUxajY")
 
-silver_linings = media.Movie("Silver Linings Playbook","A couple of strangers with mental problems that fell in love.",
-                             "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Silver_Linings_Playbook_Poster.jpg/220px-Silver_Linings_Playbook_Poster.jpg",
+silver_linings = media.Movie("Silver Linings Playbook", "A couple of strangers \
+                             with mental problems that fell in love.",
+                             "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Silver_Linings_Playbook_Poster.jpg/220px-Silver_Linings_Playbook_Poster.jpg",  # noqa
                              "www.youtube.com/watch?v=Lj5_FhLaaQQ")
 
-movies = [robocop, sixth_sense, starwars, a_beautiful_mind, others, silver_linings]
+movies = [robocop, sixth_sense, starwars, a_beautiful_mind, others,
+          silver_linings]
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,13 +1,18 @@
 __author__ = 'eduar'
 
-class Movie():
-    """This class provides the minimal requirements to create instances of the type movie.
 
-    It has as attributes the title of the movie, a storyline giving a brief of the movie itself,
-    a image URL which links to the movie poster, and a youtube link for the movies's trailer.
+class Movie():
+    """This class provides the minimal requirements to create movie instances.
+
+    It has as attributes the title of the movie, a storyline giving a brief of
+    the movie itself,
+    a image URL which links to the movie poster, and a youtube link for the
+    movies's trailer.
     """
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
